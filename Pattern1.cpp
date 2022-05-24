0f109ba9bc5dcd3ac57d7fbe797354de0cec0053
The Inspired Conjurer <ramyakrohanmohanty@gmail.com>
2022-05-24 22:38:24 +0530

    Fix code

--- Pattern1.cpp
+++ Pattern1.cpp
@@ -7,7 +7,7 @@
     *****
 
 */
-
+ 
 #include<iostream>
 
 int main()
@@ -19,9 +19,11 @@ int main()
 
   std:: cin >> n;
 
+  std::cout << std::endl;
+
   for (int row = 0; row < n; row++)
   {
-    for (int col = 0; col < row; col++)
+    for (int col = 0; col <= n; col++)
     {
 
       std::cout << "*";
