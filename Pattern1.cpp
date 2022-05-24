7492dec9db1751b6a247ec29807ca98b398fce30
The Inspired Conjurer <ramyakrohanmohanty@gmail.com>
2022-05-24 22:26:43 +0530

    Add Questions and readme

---
+++ Pattern1.cpp
@@ -0,0 +1,35 @@
+/*
+
+    *****
+    *****
+    *****
+    *****
+    *****
+
+*/
+
+#include<iostream>
+
+int main() {
+
+  int n;
+
+  std::cout << "Enter number of rows: ";
+
+  std:: cin >> n;
+
+  for (int row = 0; row < n; row++)
+  {
+    for (int col = 0; col < row; col++)
+    {
+
+      std::cout << "*";
+
+    }
+    
+    std::cout << std::endl;
+
+  }
+  
+
+}
