5a89e3c4dc8ec94eb353d5dde24d4c3448217494
The Inspired Conjurer <ramyakrohanmohanty@gmail.com>
2022-05-25 12:01:29 +0530

    Add Pattern 6

---
+++ Pattern6.cpp
@@ -0,0 +1,45 @@
+/*
+
+         *
+        **
+       ***
+      ****
+     *****
+
+*/
+
+#include <iostream>
+
+int main()
+{
+
+  int n;
+
+  std::cout << "Enter number of rows: ";
+
+  std::cin >> n;
+
+  std::cout << std::endl;
+
+  for (int row = 1; row <= n; row++)
+  {
+
+    for (int space = (n - 1); space > 0; space--)
+    {
+
+      std::cout << " ";
+
+    }
+
+    for (int col = 1; col <= row; col++)
+    {
+
+      std::cout << "*";
+
+    }
+
+    std::cout << std::endl;
+
+  }
+
+}
