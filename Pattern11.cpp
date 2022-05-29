6dfc1fce494248de0de22f1b8e8809e9a3cb7734
The Inspired Conjurer <ramyakrohanmohanty@gmail.com>
2022-05-29 13:00:51 +0530

    Solve Pattern 11

---
+++ Pattern11.cpp
@@ -0,0 +1,45 @@
+/*
+
+     * * * * *
+      * * * *
+       * * *
+        * *
+         *
+
+*/
+
+#include <iostream>
+
+int main()
+{
+
+  int n;
+
+  std::cout << "Enter number of rows: ";
+
+  std::cin >> n;
+
+  std::cout << std::endl;
+
+  for (int row = 1; row <= n; row++)
+  {
+
+    for (int space = 0; space < (row - 1); space++)
+    {
+
+      std::cout << " ";
+
+    }
+
+    for (int col = 1; col <= (n - row + 1); col++)
+    {
+
+      std::cout << "* ";
+
+    }
+
+    std::cout << std::endl;
+
+  }
+
+}
