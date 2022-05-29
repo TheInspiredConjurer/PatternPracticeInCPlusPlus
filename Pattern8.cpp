1746477b5f9e3ca37895c20cc799a29a581fb27c
The Inspired Conjurer <ramyakrohanmohanty@gmail.com>
2022-05-29 12:47:23 +0530

    Solve Pattern 8

--- Pattern8.cpp
+++ Pattern8.cpp
@@ -24,14 +24,14 @@ int main()
   for (int row = 1; row <= n; row++)
   {
 
-    for (int space = (n - 1); space > 0; space--)
+    for (int space = (n - row); space > 0; space--)
     {
 
       std::cout << " ";
 
     }
 
-    for (int col = 1; col <= ((2 * n) - 1) ; col++)
+    for (int col = 1; col <= ((2 * row) - 1) ; col++)
     {
 
       std::cout << "*";
