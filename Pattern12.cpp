b9c4ef89d8e8586739c19adc52f26f0f87dc1e70
The Inspired Conjurer <ramyakrohanmohanty@gmail.com>
2022-05-29 13:11:01 +0530

    Add Pattern 12

---
+++ Pattern12.cpp
@@ -0,0 +1,71 @@
+/*
+
+     * * * * *
+      * * * *
+       * * *
+        * *
+         *
+         *
+        * *
+       * * *
+      * * * *
+     * * * * *
+
+*/
+
+#include <iostream>
+
+int main()
+{
+
+  int n;
+
+  std::cout << "Enter number of rows: ";
+
+  std::cin >> n;
+
+  std::cout << std::endl;
+
+  for (int row = 1; row <= n; row++)
+  {
+
+    for (int space = 0; space < (row - 1); space++)
+    {
+
+      std::cout << " ";
+
+    }
+
+    for (int col = 1; col <= (n - row + 1); col++)
+    {
+
+      std::cout << "* ";
+
+    }
+
+    std::cout << std::endl;
+
+  }
+
+  for (int row = 1; row <= n; row++)
+  {
+
+    for (int space = (n - row); space > 0; space--)
+    {
+
+      std::cout << " ";
+
+    }
+
+    for (int col = 1; col <= row ; col++)
+    {
+
+      std::cout << "* ";
+
+    }
+
+    std::cout << std::endl;
+
+  }
+
+}
